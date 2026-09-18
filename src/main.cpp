ae83c775a37f931d3b1383341ddc6545a13c1974
Aaron Sun <Sunaaron07@gmail.com>
2026-09-18 00:02:55 -0400

    gradient loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 
 #include <iostream>
 
+constexpr int image_width = 256;
+constexpr int image_height = 256;
+
 int main() {
-  std::cout << "P3\n"
-            << "3 2\n"
-            << "255\n"
-            << "255 0 0   0 255 0   0 0 255\n"
-            << "255 255 0   255 255 255   0 0 0\n";
+  std::cout << "P3\n" << image_width << " " << image_height << '\n' << "255\n";
+  for (int i = 0; i < image_height; ++i) {
+    for (int j = 0; j < image_width; ++j) {
+      std::cout << j << " " << 0 << " " << i << "   ";
+    }
+    std::cout << '\n';
+  }
+  // std::cout << "P3\n"
+  //           << "3 2\n"
+  //           << "255\n"
+  //           << "255 0 0   0 255 0   0 0 255\n"
+  //           << "255 255 0   255 255 255   0 0 0\n";
 }
